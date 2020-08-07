cb30e951c0c4186719a427cda11a5db788307f23
bhagy <bhagy@LAPTOP-ERU773DA>
2020-08-07 21:26:41 +0530

    commit

---
+++ movie-catelog-service/src/main/java/com/capg/demo/movie/controller/MovieCatelogController.java
@@ -0,0 +1,50 @@
+package com.capg.demo.movie.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.capg.demo.movie.model.MovieCatelog;
+import com.capg.demo.movie.service.MovieCatelogService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+
+@RestController
+public class MovieCatelogController {
+	
+	@Autowired
+	MovieCatelogService service;
+	@Autowired
+	Environment env;
+	
+	public int GetPortNo() {
+		return Integer.parseInt(env.getProperty("local.server.port"));
+		
+	}
+	
+	//savedCatelog.setPort(GetPortNo());
+	
+	@GetMapping("/catelog/id/{id}")
+	//@HystrixCommand(fallbackMethod = "getMovieInfoFallback")//,
+
+	public MovieCatelog getMovieInfo(@PathVariable int id) {
+			System.out.println("I'm Here");
+			int x=5/0;
+			return service.getCatelog(id);
+	}
+	
+	@PostMapping("/catelog/add")
+
+	public MovieCatelog addMovieCatelog(@RequestBody MovieCatelog catelog) {
+		return service.addCatelog(catelog);
+	}
+	
+	public MovieCatelog getMovieInfoFallback(@PathVariable int id) {
+		return new MovieCatelog(id, "WAR");
+	}
+
+}
