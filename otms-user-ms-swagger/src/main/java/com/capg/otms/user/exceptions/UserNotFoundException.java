ea3896c09ae04888e8846d6ad83abc85c2f61669
bhagya <bhagya.ucholla123@gmail.com>
2020-09-14 14:17:21 +0530

    Added swagger to online test

---
+++ otms-user-ms-swagger/src/main/java/com/capg/otms/user/exceptions/UserNotFoundException.java
@@ -0,0 +1,9 @@
+
+	package com.capg.otms.user.exceptions;
+
+	public class UserNotFoundException extends RuntimeException {
+
+		public UserNotFoundException(String message) {
+			super(message);
+		}
+	}
